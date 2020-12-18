230a2d36acc2ace7f459fe964b9a190ef5b33f69
Vanius Oliveira <vaniusmax@gmail.com>
2020-12-18 10:41:07 -0300

    Hystrix circuitbreaker

--- hr-payroll/src/main/java/br/com/vanius/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/vanius/hrpayroll/resources/PaymentResource.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.vanius.hrpayroll.entities.Payment;
 import br.com.vanius.hrpayroll.services.PaymentService;
 
@@ -17,11 +19,22 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		
 		
 		Payment payment =  service.getPayment(workerId, days);
+		
+		
+		return ResponseEntity.ok(payment);
+	}
+	
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		
+		
+		Payment payment =  new Payment("Dior", 100.0, days);
 		
 		
 		return ResponseEntity.ok(payment);
--- hr-worker/src/main/java/br/com/vanius/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/vanius/hrworker/resources/WorkerResource.java
@@ -39,6 +39,14 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
 		
+		/*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
 		logger.info("PORT = "+ env.getProperty("local.server.port"));
 		
 		Worker worker = repo.findById(id).get();
